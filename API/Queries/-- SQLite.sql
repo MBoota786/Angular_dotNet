28f7bf1368f1873afd09cddff55bb1b3d7ce0cda
MBoota <m43577535@gmail.com>
2023-07-07 13:49:44 -0700

    Angular + DotNet by Vs Code + sqlite

---
+++ API/Queries/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"Admin");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2,"Saqib");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3,"Rashid");
---
+++ API/Startup.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace _2_Model_a_ViewData_ViewBag_TempData_Seasion
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(x=>{
+                x.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddControllers(); 
+            services.AddSwaggerGen(c=>{
+                c.SwaggerDoc("v1",new OpenApiInfo(){
+                    Title = "WebAPIv5",
+                    Version= "V1"
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(x=>x.SwaggerEndpoint("/swagger/v1/swagger.json","ok"));
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
